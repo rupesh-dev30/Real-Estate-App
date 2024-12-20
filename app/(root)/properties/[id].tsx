f94f431c01cb636fdeb6b4ecfa2eea8fe1055ad6
rupesh-dev30 <cgcrupesh@gmail.com>
2024-12-20 23:58:42 +0530

    initial setup

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { useLocalSearchParams } from "expo-router";
+
+export default function Property() {
+  const { id } = useLocalSearchParams();
+
+  return (
+    <View>
+      <Text>Property {id}</Text>
+    </View>
+  );
+}
