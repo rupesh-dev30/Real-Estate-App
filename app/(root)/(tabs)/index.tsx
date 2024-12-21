bb57d9036d29e9a1033cbb8687495227b480419f
rupesh-dev30 <cgcrupesh@gmail.com>
2024-12-21 05:42:23 +0530

    Home Screen, Debounce, Card, Filter, Search Components

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,66 @@
-import { Text, View } from "react-native";
+import { FeaturedCard, RegularCard } from "@/components/Card";
+import Filter from "@/components/Filter";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-3xl my-10 font-rubik">Welcome to EState</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start m-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                Rupesh Kumar
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+        <Search />
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex flex-row gap-5 mt-5">
+            <FeaturedCard />
+            <FeaturedCard />
+            <FeaturedCard />
+          </View>
+        </View>
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filter />
+
+        <View className="flex flex-row gap-5 mt-2">
+          <RegularCard />
+          <RegularCard />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ components/Search.tsx
@@ -0,0 +1,37 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { useDebounce, useDebouncedCallback } from "use-debounce";
+import { router, useLocalSearchParams, usePathname } from "expo-router";
+import icons from "@/constants/icons";
+
+export default function Search() {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [search, setSearch] = useState(params.query);
+
+  const debounced = useDebouncedCallback(
+    (text: string) => router.setParams({ query: text }),
+    1000
+  );
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+  };
+
+  return (
+    <View className="flex flex-row items-center justify-between w-full px-4 rounded-lg bg-accent-100 border border-primary-100 mt-5 py-2">
+      <View className="flex-1 flex flex-row items-center justify-start z-50">
+        <Image source={icons.search} className="size-5" />
+        <TextInput
+          value={search}
+          onChangeText={handleSearch}
+          placeholder="Search Something"
+          className="text-sm font-rubik text-black-300 ml-2 flex-1"
+        />
+      </View>
+      <TouchableOpacity>
+        <Image source={icons.filter} className="size-5" />
+      </TouchableOpacity>
+    </View>
+  );
+}
